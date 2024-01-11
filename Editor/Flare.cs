43ff0dd889baec861920e9f8538d7069271a9f11
Auros Nexus <41306347+Auros@users.noreply.github.com>
2024-01-11 09:05:08 -0500

    Plugin pass setup

---
+++ Editor/Flare.cs
@@ -0,0 +1,14 @@
+using nadena.dev.ndmf;
+
+[assembly: ExportsPlugin(typeof(Flare.Editor.Flare))]
+
+namespace Flare.Editor
+{
+    public class Flare : Plugin<Flare>
+    {
+        protected override void Configure()
+        {
+            
+        }
+    }
+}
