2f514d6f3b6ef76ffa18019b404831f25da013f4
Auros <41306347+Auros@users.noreply.github.com>
2024-02-07 02:16:52 -0500

    Only delete FlareModules and avoid deleting GameObjects that have components on them.

--- Editor/Passes/CleansePass.cs
+++ Editor/Passes/CleansePass.cs
@@ -1,4 +1,5 @@
-﻿using nadena.dev.ndmf;
+﻿using System.Linq;
+using nadena.dev.ndmf;
 using UnityEngine;
 
 namespace Flare.Editor.Passes
@@ -11,8 +12,16 @@ namespace Flare.Editor.Passes
         {
             var modules = context.AvatarRootObject.GetComponentsInChildren<FlareModule>(true);
             foreach (var module in modules)
-                if (module && module.gameObject)
-                    Object.DestroyImmediate(module.gameObject);
+            {
+                if (!module || !module.gameObject)
+                    continue;
+
+                // Make sure we don't delete a GameObject with stuff on it
+                var deleteSelf = module.gameObject.GetComponentsInChildren<Component>()
+                    .Any(c => c is not Transform && c is not FlareModule);
+                
+                Object.DestroyImmediate(deleteSelf ? module : module.gameObject);
+            }
         }
     }
 }
